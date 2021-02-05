cb092b194de7ad6650143ca2a364b11d3d94f6fc
Jack Tolmie <jacktolmie@hotmail.com>
2021-02-04 16:28:32 -0800

    second attempt

---
+++ app/src/main/java/com/jerkface/devprofile/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jerkface.devprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
